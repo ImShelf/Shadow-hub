24f1fe4d2a20b5b07bdb9d0b87963a12a46fe814
ImShelf <saitovkarimzan708@gmail.com>
2025-03-28 22:27:46 +0500

    Create Shadow_hub.lua
    Добавлен файл Shadow_hub.lua с базовым кодом для авто-фарма

---
+++ Shadow_hub.lua
@@ -0,0 +1,12 @@
+-- Shadow Hub (начальный код)
+local player = game.Players.LocalPlayer
+
+function autoFarm()
+    -- Пример авто-фарма
+    while true do
+        -- Логика фарма
+        wait(1)
+    end
+end
+
+autoFarm()
